5781c0d2af90ecb2eec39d44027279b909f125b2
Marko <marcosvelasquezvela123@gmail.com>
2025-03-30 08:21:27 -0400

    Cominezo

---
+++ src/app/daniel/page.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const Greeting: React.FC = () => {
+  return (
+    <div>
+      <h1>¡Hola, bienvenido!</h1>
+      <p>Espero que tengas un excelente día.</p>
+    </div>
+  );
+};
+
+export default Greeting;
