a304310707ae7b5f06f5976edf6d0b739c02aa23
Willian Andres Almendras Calizaya <willian.a.almendras@gmail.com>
2025-04-11 00:28:51 -0400

    New: Tarjeta con botones y imagen posicionado

--- src/app/musica/page.tsx
+++ src/app/musica/page.tsx
@@ -7,9 +7,6 @@ export default function Home() {
       <Header />  
         <main className="flex min-h-screen flex-col items-center  p-8 md:p-24">
           <h1 className="text-4xl font-bold text-center">Bienvenido a REDIBO</h1>
-          <p className="mt-4 text-lg">
-            Tu tienda en línea para rentar autos.
-          </p>
           <CarCard name="hola" price="99.9$" rating={9.9}/>
         </main>
     </div>
--- src/components/uiHome/CarCard.tsx
+++ src/components/uiHome/CarCard.tsx
@@ -4,12 +4,40 @@ interface CarCardProps {
   name: string;
   price: string;
   rating: number;
+
 }
 
-export default function CarCard({ name, price, rating }: CarCardProps) {
+export default function CarCard({ name, price, rating}: CarCardProps) {
   return (
-    <div className="flex gap-4 border rounded p-4 mb-4">
-      <div className="w-32 h-24 bg-gray-300 rounded"></div>
+    <div className="
+      w-[620px]              /* ancho exacto de Figma */
+      h-[250px]              /* alto exacto de Figma */
+      border                 /* borde 1px */
+      border-black        /* color de borde */
+      rounded-[15px]         /* bordes redondeados de 15px */
+      p-6                    /* padding interior */
+      shadow-sm              /* sombra suave */
+      bg-white               /* fondo blanco */
+      flex                   /* layout en fila */
+      gap-6                  /* espacio entre columnas */
+      mx-auto                /* centrado horizontal */
+    ">
+      <div className="relative w-[270px] h-[125px]">
+      <div className="absolute -top-3 left-1 flex gap-11">
+        <button className="flex items-center gap-2 px-3 py-1 border border-black rounded-full text-sm">
+          <span>♡</span> Guardar
+        </button>
+        <button className="flex items-center gap-2 px-3 py-1 border border-black rounded-full text-sm">
+          <span>↪</span> Compartir
+        </button>
+      </div>
+        <div className="absolute top-7 left-1 w-[250px] h-[150px] bg-gray-200 rounded-[10px] text-center">
+          🚗 Imagen aquí
+        </div>
+      </div>
+
+
+
       <div className="flex-1">
         <h2 className="font-bold text-lg">{name}</h2>
         <p className="text-sm">⭐ {rating} / 10</p>
