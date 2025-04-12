145509dacd9aa8e75bde5ec521cb9e916e62d83f
Ronald Krings Revollo <202202861@est.umss.edu>
2025-04-11 23:41:07 -0400

    Revert "Merge branch '7-bits' of https://github.com/Dauphinsss/Redibo-Frontend into 7-bits"
    This reverts commit cdb2e262eb005bb7d74cd2505199024bcfbe1f83, reversing
    changes made to ee3ec2ce67b25ccac4b919fbb107c74a7cef6916.

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,11 @@
 
 import CarsCarousel from "@/components/custom/CarsCarousel";
-
+import Header from "@/components/ui/Header";
 
 export default function Home() {
   return (
     <div className="min-h-screen flex flex-col">
+      <Header />
       <main className="flex flex-col items-center justify-center flex-1 px-4 py-8 sm:px-6 md:px-12 lg:px-24">
         <h1 className="text-3xl sm:text-4xl lg:text-5xl font-bold text-center">
           Bienvenido a REDIBO
