7a75cb79b7a7f698d4c6c80ffc45489bf6224852
alejandro <alejandrotorrico743@gmail.com>
2025-04-12 01:19:09 -0400

    pantalla de login

---
+++ src/app/login/page.tsx
@@ -0,0 +1,9 @@
+const WelcomeComponent: React.FC = () => {
+return (
+    <div style={{ textAlign: 'center', marginTop: '50px' }}>
+        <h1>Falta implemetar</h1>
+    </div>
+);
+};
+
+export default WelcomeComponent;
