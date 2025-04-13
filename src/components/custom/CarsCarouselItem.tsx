fb24e31d577558d8a4cd3ec7fb1d5e646f594677
efrain <efrainperalta.f@gmail.com>
2025-04-13 07:20:43 -0400

    fix: correccion de nombre en carrusel item

--- src/components/custom/CarsCarouselItem.tsx
+++ src/components/custom/CarsCarouselItem.tsx
@@ -3,19 +3,19 @@ import { Card, CardContent } from '@/components/ui/card'
 import { CarouselItem } from '@/components/ui/carousel'
 import ReservationConfirmedMessage from './ReservationConfirmation/ReservationConfirmedMessage'
 
-function CarsCarouselItem({ car }: { car: { marca: string; modelo: string; anio: string ; price: number ; image: string; vecesAlquilado: number} }) {
+function CarsCarouselItem({ car }: { car: { marca: string; modelo: string; anio: string ; precio_por_dia: number ; imagenes: string; veces_alquilado: number} }) {
 
   return (
     <CarouselItem className="pl-5 md:basis-1/3 basis-full">
       <Card className="relative rounded-3x shadow-xl overflow-hidden transition-shadow duration-300 hover:shadow-2xl p-0 gap-0">
         <div className="absolute top-4 left-4 flex flex-col items-start z-10">
           <span className="bg-black text-white px-3 py-1 text-xs font-semibold rounded-full shadow">
-            ${car.price}
+            ${car.precio_por_dia}
           </span>
         </div>
 
         <div className="h-55 md:h-40 overflow-hidden bg-gray-200 rounded-3x">
-          <img src={car.image} alt={car.modelo} className="w-full h-full object-cover transition-transform duration-300 ease-in-out transform hover:scale-105" />
+          <img src={car.imagenes} alt={car.modelo} className="w-full h-full object-cover transition-transform duration-300 ease-in-out transform hover:scale-105" />
         </div>
 
         <CardContent className="p-4 bg-white">
@@ -26,7 +26,7 @@ function CarsCarouselItem({ car }: { car: { marca: string; modelo: string; anio:
           <div className="flex items-center">
             <span className="text-sm text-gray-500">Alquilado:</span>
             <span className="ml-2 bg-blue-200 text-blue-700 text-xs font-semibold rounded-full px-3 py-1">
-              {car.vecesAlquilado} veces
+              {car.veces_alquilado} veces
             </span>
           </div>
           <ReservationConfirmedMessage />
