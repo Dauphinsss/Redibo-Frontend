548fc2455412a645bcadb8284f22e5676dd44239
JHOSEPH <jhoseph@localhost.localdomain>
2025-04-15 19:35:39 -0400

    add: service enpoint

---
+++ src/service/serviceRecode.ts
@@ -0,0 +1,78 @@
+import api from "@/api/api";
+
+export const getAllCars = async () => {
+  try {
+    const response = await api.get("/autos");
+    return response.data;
+  } catch (error) {
+    console.error("Error al obtener los autos:", error);
+    throw error;
+  }
+};
+
+export const getCarById = async (id: string) => {
+  try {
+    const response = await api.get(`/detailCar/${id}`);
+    return response.data;
+  } catch (error) {
+    console.error(`Error al obtener el auto con ID ${id}:`, error);
+    throw error;
+  }
+};
+
+// Filtrar autos por modelo descendente
+export const getCarsByModelDesc = async () => {
+  try {
+    const response = await api.get("/filterCar", {
+      params: {
+        ordenar: "modelo_desc",
+      },
+    });
+    return response.data;
+  } catch (error) {
+    console.error("Error al filtrar autos por modelo descendente:", error);
+    throw error;
+  }
+};
+
+export const getCarsByModelAsc = async () => {
+  try {
+    const response = await api.get("/filterCar", {
+      params: {
+        ordenar: "modelo_asc",
+      },
+    });
+    return response.data;
+  } catch (error) {
+    console.error("Error al filtrar autos por modelo ascendente:", error);
+    throw error;
+  }
+};
+
+export const getCarsByPriceAsc = async () => {
+  try {
+    const response = await api.get("/filterCar", {
+      params: {
+        ordenar: "precio_asc",
+      },
+    });
+    return response.data;
+  } catch (error) {
+    console.error("Error al filtrar autos por precio ascendente:", error);
+    throw error;
+  }
+};
+
+export const getCarsByPriceDesc = async () => {
+  try {
+    const response = await api.get("/filterCar", {
+      params: {
+        ordenar: "precio_desc",
+      },
+    });
+    return response.data;
+  } catch (error) {
+    console.error("Error al filtrar autos por precio descendente:", error);
+    throw error;
+  }
+};
