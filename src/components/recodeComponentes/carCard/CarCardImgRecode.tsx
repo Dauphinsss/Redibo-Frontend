a5ac9fecb54583c682b732da22f19bf7b5be03e8
Willian Andres Almendras Calizaya <willian.a.almendras@gmail.com>
2025-04-19 18:20:45 -0400

    Fix(CarImg): se mejoro

--- src/components/recodeComponentes/carCard/CarCardImgRecode.tsx
+++ src/components/recodeComponentes/carCard/CarCardImgRecode.tsx
@@ -9,19 +9,20 @@ interface Props {
 export default function CarCardImg({ imagenUrl }: Props) {
     return (
         <div className="w-[250px] h-[250px] rounded-xl flex items-center justify-center">
-            <div className="w-[250px] h-[150px] rounded-[10px] overflow-hidden bg-gray-200 flex items-center justify-center">
-                {imagenUrl ? (
-                    <Image
-                    src={imagenUrl}
-                    alt="Imagen del auto"
-                    width={250}
-                    height={150}
-                    className="w-full h-full object-cover"
-                    />
-                ) : (
-                    <span className="text-sm bg-gray-100 text-black">Sin imagen</span>
-                )}
-                </div>
+        <div className="w-[250px] h-[150px] rounded-[10px] overflow-hidden bg-gray-200 flex items-center justify-center">
+            {imagenUrl ? (
+            <Image
+                src={imagenUrl}
+                alt="Imagen del auto"
+                width={230}
+                height={150}
+                className="object-contain"
+                priority // mejora rendimiento en páginas visibles
+            />
+            ) : (
+            <span className="text-sm bg-gray-100 text-black">Sin imagen</span>
+            )}
+        </div>
         </div>
     );
 }
