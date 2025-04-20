c87d080b09c927373d3b8355db861850d03b124b
neverdead1 <55112480+neverdead1@users.noreply.github.com>
2025-04-20 12:45:55 -0400

    Fix(SearchBar): ocultar boton al buscar

--- src/components/recodeComponentes/RecodeSearchBar.tsx
+++ src/components/recodeComponentes/RecodeSearchBar.tsx
@@ -10,10 +10,14 @@ interface SearchBarProps {
 
 const SearchBar: React.FC<SearchBarProps> = ({ placeholder, onFiltrar }) => {
   const [busqueda, setBusqueda] = useState("");
+  const [mostrarBoton, setMostrarBoton] = useState(true);
 
-  const [mostrarBoton, setMostrarBoton] = useState(false);
-  
-  //Aplica debounce de 300ms a la búsqueda
+  // Mostrar el botón si hay texto
+  useEffect(() => {
+    setMostrarBoton(busqueda.trim().length === 0);
+  }, [busqueda]);
+
+  // Debounce de 300ms
   useEffect(() => {
     const timer = setTimeout(() => {
       const valorNormalizado = busqueda.trim().replace(/\s+/g, " ").toLowerCase();
@@ -31,11 +35,14 @@ const SearchBar: React.FC<SearchBarProps> = ({ placeholder, onFiltrar }) => {
         aria-label="Campo de búsqueda de autos por modelo o marca"
         value={busqueda}
         onChange={(e) => setBusqueda(e.target.value)}
+        onFocus={() => setMostrarBoton(false)}
+        onBlur={() => setMostrarBoton(true)}
         className="p-2 border border-gray-300 rounded-md w-full h-12 text-left pr-12 text-[11px] md:text-base lg:text-lg"
       />
       {mostrarBoton && (
         <button
           type="button"
+          aria-label="Buscar autos"
           className="absolute right-1 top-1/2 transform -translate-y-1/2 w-10 h-10 bg-black text-white rounded-md flex items-center justify-center"
         >
           <MagnifyingGlassIcon className="h-5 w-5" />
