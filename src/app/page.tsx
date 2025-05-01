b3b330c5f41b9fa052f2184778b13642161ea278
Daniel Virreira <virreira.daniel@gmail.com>
2025-05-01 11:26:54 -0400

    Eliminar barra busqueda de home

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -10,7 +10,6 @@ export default function Home() {
   return (
     <div>
       <Header />
-      <SearchBar />
       <main className="flex min-h-screen flex-col items-center p-8 md:p-24">
         <h1 className="text-4xl font-bold text-center">Bienvenido a REDIBO</h1>
         <p className="mt-4 text-lg">
