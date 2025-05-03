a3bbf40801707f48fa91ad5cc1e3901c841fff4d
Willian Andres Almendras Calizaya <willian.a.almendras@gmail.com>
2025-05-03 03:44:31 -0400

    Refactor interfaces y funciones de transformación para unificar nombres de propiedades y mejorar la consistencia

--- src/utils/transformAuto_Recode.ts
+++ src/utils/transformAuto_Recode.ts
@@ -1,4 +1,4 @@
-import { RawAuto_Interface_Recode as RawAuto} from "@/interface/RawAuto_Interface_Recode";
+import { RawAuto_Interface_Recode as RawAuto } from "@/interface/RawAuto_Interface_Recode";
 import { AutoCard_Interfaces_Recode as AutoCard } from "@/interface/AutoCard_Interface_Recode";
 
 export const transformAuto = (item: RawAuto): AutoCard => ({
@@ -7,19 +7,19 @@ export const transformAuto = (item: RawAuto): AutoCard => ({
     marca: item.marca,
     asientos: item.asientos,
     puertas: item.puertas,
-    transmision: item.transmision,
-    combustibles: Array.isArray(item.combustiblecarro)
-        ? item.combustiblecarro
-            .map((c) => c?.tipocombustible?.tipo_de_combustible)
+    transmision: item.transmicion,
+    combustibles: Array.isArray(item.CombustibleCarro)
+        ? item.CombustibleCarro
+            .map((c) => c?.TipoCombustible?.tipoDeCombustible)
             .filter((c): c is string => typeof c === "string")
         : [],
     estadoAlquiler: item.estado,
-    nombreHost: item.usuario_rol?.usuario?.nombre || "Sin nombre",
+    nombreHost: item.Usuario?.nombre || "Sin nombre",
     calificacionAuto: 4.5,
-    ciudad: item.direccion?.provincia?.ciudad?.nombre || "Desconocido",
-    calle: item.direccion?.calle || "No especificada",
+    ciudad: item.Direccion?.Provincia?.Ciudad?.nombre || "Desconocido",
+    calle: item.Direccion?.calle || "No especificada",
     precioOficial: Number(item.precio_por_dia),
     precioDescuento: Number(item.precio_por_dia),
     precioPorDia: Number(item.precio_por_dia),
-    imagenURL: item.imagen?.[0]?.data || "",
+    imagenURL: item.Imagen?.[0]?.data || "",
 });
