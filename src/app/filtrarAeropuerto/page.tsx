7a3994e2513cf9e72a7a8901f7673d3a57401154
Ronald Krings Revollo <202202861@est.umss.edu>
2025-05-06 09:45:02 -0400

    fix: aeropuertos no se mostraban en el select

--- src/app/filtrarAeropuerto/page.tsx
+++ src/app/filtrarAeropuerto/page.tsx
@@ -1,23 +1,27 @@
 "use client"
-import React from 'react'
-import { useState } from 'react';
-import {useAirports } from '@/api/queries/useAirports'
+//import React from 'react'
+import { useState, useEffect } from 'react';
+
+//import {useAirports } from '@/api/queries/useAirports'
 import { Button } from "@/components/ui/button"
 import CarsByLocation from '@/components/custom/CarsByLocation';
 
 export default function Page() {  
-  const { data: content = [], isLoading, isError } = useAirports();
+  //const { data: content = [], isLoading, isError } = useAirports();
+  const [data, setData] = useState<any[]>([]);
+  const [loading, setLoading] = useState(true);  
   let [selectedValue, setSelectedValue] = useState('0');
   let [selectedLatitude, setSelectedLatitude] = useState(0);
   let [selectedLongitude, setSelectedLongitude] = useState(0);
  const handleClick = () => {
-    const index = parseInt(selectedValue);
-    const latitude = content[index].latitud
-    setSelectedLatitude(latitude);
-    const longitude = content[index].longitud
-    setSelectedLongitude(longitude);    
+    const index = parseInt(selectedValue);    
+    const lat = data[index].latitude;
+    setSelectedLatitude(lat);
+    const long = data[index].longitude;
+    setSelectedLongitude(long);    
+    
   }
-  const cities = new Map([
+  /*const cities = new Map([
     [1, "Beni"],
     [2, "Chuquisaca"],
     [3, "Cochabamba"],
@@ -27,18 +31,44 @@ export default function Page() {
     [7, "Potosí"],
     [8, "Santa Cruz"],
     [9, "Tarija"]
-  ]);
+  ]);*/
+  useEffect(() => {
+    async function fetchData() {
+      try {
+        const response = await fetch('/data/aeropuertos.json'); // Assuming data.json is in the public directory
+        if (!response.ok) {
+          throw new Error(`HTTP error! status: ${response.status}`);
+        }
+        const json = await response.json();
+        setData(json);
+      } catch (e) {
+        //setError(e);
+      } finally {
+        setLoading(false);
+      }
+    }
+    fetchData();
+  }, []);
+
+  if (loading) {
+    return <p>Loading data...</p>;
+  }
+
+  /*if (error) {
+    return <p>Error</p>;
+  }*/
+
   return (
     <div className="max-w-xl mx-auto pt-4">
         <h1 className="text-center text-2xl mb-4 font-semibold">Filtar Por Aeropuerto</h1>
         <p className="mb-4 font-semibold">Seleccione un Aeropuerto</p>
         <div className="flex gap-1 mb-4">
-          <select id="aeropuertos" className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2"
+          <select id="aeropuertos" className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2 me-1"
           value={selectedValue} onChange={(e) => setSelectedValue(e.target.value)}
           >
-          {content.map((item:any, i:number) => (
-          <option key={i} value={i} data-latitude={item.latitud} data-longitude={item.longitud}>
-              {item.nombre} ({cities.get(item.id_ciudad)}) 
+          {data.map((item:any, i:number) => (
+          <option key={i} value={i} data-latitude={item.latitude} data-longitude={item.longitude}>
+              {item.name} ({item.location}) 
           </option>
           ))}
           </select>
--- src/components/custom/CarsByLocation.tsx
+++ src/components/custom/CarsByLocation.tsx
@@ -43,7 +43,7 @@ const CarsByLocation = ({latitude, longitude}:any) => {
   }*/
 
   return (    
-  <ul className="max-w-lg divide-y divide-gray-200 dark:divide-gray-700">
+  <ul className="max-w-xl divide-y divide-gray-200 dark:divide-gray-700">
     {data.map((item:any, i:number) => {
       const distance =  haversineDistance(latitude, longitude, item.latitud, item.longitud);   
      
@@ -52,19 +52,25 @@ const CarsByLocation = ({latitude, longitude}:any) => {
       <li key={i} className="pb-3 sm:pb-4">
         <div className="flex items-center space-x-4 rtl:space-x-reverse">
          <div className="shrink-0">
-            <img className="w-8 h-8 rounded-full" src="https://placehold.co/64" alt="Neil image" />
+            <img className="w-16 h-16 rounded-sm" src="https://placehold.co/64" alt="imagen auto" />
          </div>
          <div className="flex-1 min-w-0">
             <p className="text-sm font-semibold text-gray-900 truncate dark:text-white">
               {item.nombre}
             </p>
-            <p className="text-sm text-gray-500 truncate dark:text-gray-400">
-            {item.marca} distancia: ({distance})
+            <p className="text-sm text-gray-500 truncate">
+            Marca:{item.marca}  &#124; {item.calificacion} &#9733; &#124; distancia: ({distance} Km)
+            </p>
+            <p className="text-sm text-gray-800 truncate">
+            Asientos:{item.asientos} &#124; puertas: {item.puertas} &#124; transmision: {item.transmision} 
             </p>
          </div>
-         <div className="inline-flex items-center text-base font-semibold text-gray-900 dark:text-white">
+         <div className="inline-flex items-center text-base font-semibold text-gray-900">
             {item.precioOficial}
+            
          </div>
+         <button type="button" className="text-gray-900 bg-white border border-gray-300 focus:outline-none hover:bg-gray-100 focus:ring-4 focus:ring-gray-100 font-xs rounded-lg text-xs px-3 py-2 me-2 mb-2">
+          Ver Detalles</button>
       </div>        
       </li>)
       }  
