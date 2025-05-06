731e371bbf87c9071188b800b4e6b52354837bff
Willian Andres Almendras Calizaya <willian.a.almendras@gmail.com>
2025-05-06 01:50:59 -0400

    feat(General_recode): se puso los radio buttos y los inputs

--- src/app/condicionUsoAuto/CondicionesUsoAutoHome.tsx
+++ src/app/condicionUsoAuto/CondicionesUsoAutoHome.tsx
@@ -1,18 +1,11 @@
 'use client'
-import RadioGroup_Recode from "@/components/recodeComponentes/condicionesDeUsoAuto/condicioneGenerales/RadioGroup_Recode";
-import { useState } from "react";
+import General_Recode from "@/components/recodeComponentes/condicionesDeUsoAuto/condicioneGenerales/General_Recode";
 
 export default function CondicionesUsoAutoHome() {
-    const [respuesta, setRespuesta] = useState("si");
 
     return (
         <div className="p-4">
-            <h1 className="text-lg font-semibold mb-4">¿Acepta los términos?</h1>
-            <RadioGroup_Recode
-                value={respuesta}
-                onChange={setRespuesta}
-                name="aceptacion"
-            />
-    </div>
+            <General_Recode/>
+        </div>
     );
 }
