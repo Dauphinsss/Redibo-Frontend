23fe4720e279e4a5201a6b61d5d70a3de3f479cd
Edmilzon <edmilzon.luna@gmail.com>
2025-05-13 17:51:34 -0400

    solucion Notificaion Envio

--- src/components/recodeComponentes/condicionesDeUsoAutoVisual/Notificacion_envio_exitoso_Recode.tsx
+++ src/components/recodeComponentes/condicionesDeUsoAutoVisual/Notificacion_envio_exitoso_Recode.tsx
@@ -1,5 +1,5 @@
 import React from "react";
-import { FiBell } from "react-icons/fi"; // Importa el ícono de campana
+import { FiBell } from "react-icons/fi";
 
 interface NotificacionEnvioExitosoProps {
   onClose: () => void;
@@ -8,10 +8,9 @@ interface NotificacionEnvioExitosoProps {
 export default function NotificacionEnvioExitoso_recode({ onClose }: NotificacionEnvioExitosoProps) {
   return (
     <div className="fixed inset-0 z-50 bg-black/20 pointer-events-none">
-      {/* Notificación en la parte superior */}
       <div className="absolute top-4 left-1/2 transform -translate-x-1/2 bg-white border border-gray-300 shadow-lg rounded-lg p-4 max-w-md w-full z-50 pointer-events-auto text-center">
         <div className="flex items-center justify-center mb-2">
-          <FiBell className="text-black text-2xl mr-2" /> {/* Ícono al lado del título */}
+          <FiBell className="text-black text-2xl mr-2" />
           <h2 className="text-black font-bold text-lg">Solicitud enviada con éxito</h2>
         </div>
         <p className="text-gray-700 text-sm mb-4">
