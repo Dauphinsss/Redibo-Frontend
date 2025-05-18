f0791b9f30df25e19e83a2f9ce842bd85a1b7741
Franklin Garcia <202202521@est.umss.edu>
2025-05-18 10:27:55 -0400

    correccion de componente sprinteros- badge.tsx

--- src/components/ui/badge.tsx
+++ src/components/ui/badge.tsx
@@ -1,28 +1,3 @@
-<<<<<<< HEAD
-import { cn } from "@/lib/utils";
-import * as React from "react";
-
-export function Badge({
-  className,
-  variant = "default",
-  ...props
-}: React.HTMLAttributes<HTMLDivElement> & { variant?: "default" | "secondary" | "destructive" }) {
-  return (
-    <div
-      className={cn(
-        "inline-flex items-center rounded-md px-2.5 py-0.5 text-xs font-semibold transition-colors",
-        {
-          "bg-primary text-primary-foreground": variant === "default",
-          "bg-secondary text-secondary-foreground": variant === "secondary",
-          "bg-destructive text-destructive-foreground": variant === "destructive",
-        },
-        className
-      )}
-      {...props}
-    />
-  );
-}
-=======
 import * as React from "react"
 import { Slot } from "@radix-ui/react-slot"
 import { cva, type VariantProps } from "class-variance-authority"
@@ -70,4 +45,3 @@ function Badge({
 }
 
 export { Badge, badgeVariants }
->>>>>>> origin/sprinteros
