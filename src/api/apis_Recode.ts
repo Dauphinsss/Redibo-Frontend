40a60d3ef856ce3cc77f67538afd9a34ee38308d
camilonogales96 <nogales.fuentes.camilo.baltazar@gmail.com>
2025-05-19 02:48:16 -0400

    add: Agregado de apis

---
+++ src/api/apis_Recode.ts
@@ -0,0 +1,35 @@
+import axios from "axios";
+
+const apiAllCards = axios.create({
+    baseURL: "https://search-car-backend.vercel.app/searchCar",
+    headers: {
+        "Content-Type": "application/json",
+    },
+});
+
+const apiCarById = axios.create({
+    baseURL: "https://search-car-backend.vercel.app",
+    headers: {
+        "Content-Type": "application/json",
+    },
+});
+
+const apiFormularioCondicionesUsoAuto = axios.create({
+    baseURL: "https://search-car-backend.vercel.app",
+    headers: {
+        "Content-Type": "application/json",
+    },
+});
+
+const apiCobertura = axios.create({
+    baseURL: "https://search-car-backend.vercel.app",
+    headers:{
+        "Content-Type": "application/json",
+    }
+});
+
+const getCondicionesUsoAutoAPI = (id_carro: number) => {
+    return apiCarById.get(`/useConditon/${id_carro}`);
+};
+
+export { apiAllCards, apiCarById, apiFormularioCondicionesUsoAuto, getCondicionesUsoAutoAPI, apiCobertura };
