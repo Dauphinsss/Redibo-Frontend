357b4acf375e7636ca15983becef27284e81971f
neverdead1 <55112480+neverdead1@users.noreply.github.com>
2025-05-21 00:01:10 -0400

    Refactor:Reserva reubicacion carpetas integracion

--- src/app/reserva/components/componentes_InfoAutp_Recode/detailsCar/RecodeReserva.tsx
+++ src/app/reserva/components/componentes_InfoAutp_Recode/detailsCar/RecodeReserva.tsx
@@ -6,7 +6,7 @@ export default function Reserva({ id, precio }: ReservaProps) {
     <div className="w-full border border-gray-300 rounded-lg p-4 text-center">
       <p className="text-lg font-bold">BOB. {precio}</p>
       <p className="text-sm text-gray-500">Precio por dia</p>
-      <Link href={`/condicionesVisual_Recode/${id}`} target="_blank">
+      <Link href={`/reserva/page/condicionesVisual_Recode/${id}`} target="_blank">
         <button className="mt-2 w-full py-2 bg-black text-white rounded-lg hover:bg-gray-600 transition duration-300">
           Reserva
         </button>
