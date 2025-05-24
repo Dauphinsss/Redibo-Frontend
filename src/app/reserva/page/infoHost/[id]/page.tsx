baa53c935390870c8a3da6059115cbcba05c856f
neverdead1 <55112480+neverdead1@users.noreply.github.com>
2025-05-24 17:42:56 -0400

    Refactor:Parte estetica de detalle host

--- src/app/reserva/page/infoHost/[id]/page.tsx
+++ src/app/reserva/page/infoHost/[id]/page.tsx
@@ -35,8 +35,8 @@ export default function Page() {
 
   return (
     <div className="p-6 max-w-6xl mx-auto">
-      <div className="sticky top-0 z-50 bg-white shadow overflow-visible">
-              <div className="border-b px-4 sm:px-6 lg:px-8 py-7">
+      <div className="sticky top-0 z-50 bg-white overflow-visible">
+              <div className=" px-4 sm:px-6 lg:px-8 py-7">
                 <Header />
               </div>
             </div>
