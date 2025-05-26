5759bd77b1952594aed8036948ba18b069f18acc
Daniel Virreira <virreira.daniel@gmail.com>
2025-05-26 15:36:19 -0400

    Eliminar bucle infinito filtros por generacion de bugs

--- src/app/busqueda/components/fitroCarusel/infinite-filter-carousel.tsx
+++ src/app/busqueda/components/fitroCarusel/infinite-filter-carousel.tsx
@@ -185,7 +185,7 @@ export function InfiniteFilterCarousel({
         className="w-full"
         opts={{
           align: 'start',
-          loop: true,
+          loop: false,
           dragFree: true,
           containScroll: 'trimSnaps' // Mejora el comportamiento del scroll
         }}
