3002cf81ecb3d2c98cf1fdfedb6cc687809bcaad
ccdana <danacalustro@gmail.com>
2025-05-27 23:05:10 -0400

    Se añadio la gestion de estados para reaccionar a los cambios

--- src/app/busqueda/hooks/customSearchHU/useCustomSearch.tsx
+++ src/app/busqueda/hooks/customSearchHU/useCustomSearch.tsx
@@ -1,6 +1,12 @@
+import { useEffect, useState } from "react";
 import { AutoCard_Interfaces_Recode as Auto } from "@/app/busqueda/interface/AutoCard_Interface_Recode";
 
 export function useCustomSearch(autosFiltrados: Auto[], busqueda: string) {
-  // Devuelve la lista sin filtrar por ahora
-  return autosFiltrados;
+  const [autosBuscados, setAutosBuscados] = useState<Auto[]>(autosFiltrados);
+
+  useEffect(() => {
+    // La lógica de filtrado vendrá después
+  }, [autosFiltrados, busqueda]);
+
+  return autosBuscados;
 }
