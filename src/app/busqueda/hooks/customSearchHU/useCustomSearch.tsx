549de1805be66d7e6ce7ec5db1ed6f895f68a89b
ccdana <danacalustro@gmail.com>
2025-05-27 23:01:40 -0400

    Logica del hook

---
+++ src/app/busqueda/hooks/customSearchHU/useCustomSearch.tsx
@@ -0,0 +1,6 @@
+import { AutoCard_Interfaces_Recode as Auto } from "@/app/busqueda/interface/AutoCard_Interface_Recode";
+
+export function useCustomSearch(autosFiltrados: Auto[], busqueda: string) {
+  // Devuelve la lista sin filtrar por ahora
+  return autosFiltrados;
+}
