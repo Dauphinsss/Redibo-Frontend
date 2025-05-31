bfaa73e86be0a30d34f05a685479fafb14d9f9f0
neverdead1 <55112480+neverdead1@users.noreply.github.com>
2025-05-30 22:49:41 -0400

    "Nueva solicitud de alquiler"

--- next.config.ts
+++ next.config.ts
@@ -26,6 +26,14 @@ const nextConfig = {
         protocol: "https",
         hostname: "lh3.googleusercontent.com",
       },
+      {
+        protocol: "https",
+        hostname: "images.pexels.com",
+      },
+      {
+        protocol: "https",
+        hostname: "media.istockphoto.com",
+      }
     ],
   },
 };
