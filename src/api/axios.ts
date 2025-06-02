ba1d15c7b5163e3c100269fc2edab7913b535864
efrain <efrainperalta.f@gmail.com>
2025-06-02 18:06:44 -0400

    correccion

--- src/api/axios.ts
+++ src/api/axios.ts
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const axiosInstance = axios.create({
-  baseURL: "https://is-ps-back.vercel.app/",
+  baseURL: "https://backend-is.vercel.app/",
   timeout: 5000,
   headers: {
     "Content-Type": "application/json",
