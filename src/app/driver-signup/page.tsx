69094b99e5e2e4448e3b15c987688faee91ef842
Marko 3 <marcosvelasquezvela123@gmail.com>
2025-06-01 20:50:08 -0400

    bug router

--- src/app/driver-signup/page.tsx
+++ src/app/driver-signup/page.tsx
@@ -3,12 +3,27 @@
 import Header from "@/components/ui/Header";
 import DriverApplicationForm from "./driver-application-form";
 import { Footer } from "@/components/ui/footer";
+import { useEffect, useState } from "react";
+import { useRouter } from "next/navigation";
 
 export default function ConductorPage() {
+  const [estado, setEstado] = useState("NO_REQUESTED");
+  const router = useRouter();
+
+  useEffect(() => {
+    const storedState = localStorage.getItem("estadoConductor");
+    if (storedState) {
+      setEstado(storedState);
+    }
+    if (estado !== "REQUESTED") {
+      router.push("/");
+    }
+  }, []);
+
   return (
     <div className="flex flex-col min-h-screen">
       <Header />
-      <DriverApplicationForm />;
+      <DriverApplicationForm />
       <Footer />
     </div>
   );
