ee4320414ab63b74132513c043188822d2bdeca8
Willian Andres Almendras Calizaya <willian.a.almendras@gmail.com>
2025-06-04 02:27:02 -0400

    Añadir rol de arrendatario en el diálogo de reporte de perfil en RenterDetails.

--- src/app/usuario/[id]/page.tsx
+++ src/app/usuario/[id]/page.tsx
@@ -298,6 +298,7 @@ export default function RenterDetails() {
                 <ReportProfileDialog
                   renterId={String(renterDetails.id)}
                   renterName={`${renterDetails.firstName} ${renterDetails.lastName}`}
+                  renterRole="RENTER"
                 >
                   <Button
                     variant="outline"
