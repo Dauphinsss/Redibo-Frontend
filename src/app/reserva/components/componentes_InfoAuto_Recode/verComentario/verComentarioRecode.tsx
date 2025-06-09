b4f86a16d9b5f209ebdc779115ff42676a89a7f9
ANGEL MAMANI <202108255@est.umss.edu>
2025-06-08 20:53:53 -0400

    Refactor VerComentario: Añadir funcionalidad de eliminación de comentarios y ajustar props

--- src/app/reserva/components/componentes_InfoAuto_Recode/verComentario/verComentarioRecode.tsx
+++ src/app/reserva/components/componentes_InfoAuto_Recode/verComentario/verComentarioRecode.tsx
@@ -4,6 +4,9 @@ import { FaStar } from "react-icons/fa";
 import { HiOutlineChat, HiThumbUp, HiThumbDown } from "react-icons/hi";
 
 interface Props {
+  idComentario: number;
+  idUsuarioComentario: number;
+  userId: number; 
   nombreCompleto: string;
   fotoUser: string;
   fechaComentario: string;
@@ -11,13 +14,24 @@ interface Props {
   calificacionUsr: number;
   cantLikes: number;
   cantDontlikes: number;
+  onEliminar: (id: number) => void;
 }
 
-function VerComentario({nombreCompleto, fotoUser, fechaComentario, comentario, calificacionUsr,cantLikes,cantDontlikes}: Props) {
-  
+function VerComentario({
+  idComentario,
+  idUsuarioComentario,
+  userId,
+  nombreCompleto,
+  fotoUser,
+  fechaComentario,
+  comentario,
+  calificacionUsr,
+  cantLikes,
+  cantDontlikes,
+  onEliminar,
+}: Props) {
   return (
     <div className="w-full border border-gray-200 rounded-lg p-4">
-
       <div className="flex flex-col md:flex-row md:justify-between gap-4">
         <div className="flex gap-4 items-start">
           <FotoPerfilUsr imagenUrl={fotoUser} ancho={50} alto={50} />
@@ -33,6 +47,19 @@ function VerComentario({nombreCompleto, fotoUser, fechaComentario, comentario, c
 
         <div className="text-sm text-right md:text-left">
           <p>{fechaComentario}</p>
+          {userId === idUsuarioComentario && (
+            <button
+              onClick={() => {
+                const confirmacion = confirm(
+                  "¿Deseas eliminar este comentario?"
+                );
+                if (confirmacion) onEliminar(idComentario);
+              }}
+              className="text-red-600 text-sm hover:underline"
+            >
+              Eliminar
+            </button>
+          )}
         </div>
       </div>
 
@@ -45,13 +72,13 @@ function VerComentario({nombreCompleto, fotoUser, fechaComentario, comentario, c
       </div>*/}
 
       <div className="flex flex-col sm:flex-row sm:justify-between sm:items-center gap-4 ">
-        <button className="flex items-center gap-2 px-4 py-2 text-gray-600 rounded-lg self-start sm:self-auto">{/*hover:bg-stone-300*/}
+        <button className="flex items-center gap-2 px-4 py-2 text-gray-600 rounded-lg self-start sm:self-auto">
+          {/*hover:bg-stone-300*/}
           <HiOutlineChat />
           Comentar
         </button>
 
         <div className="flex items-center space-x-2 text-sm self-start sm:self-auto">
-
           <div className="flex items-center gap-1 cursor-pointer text-gray-400 hover:text-gray-600">
             <HiThumbUp />
             <span>{cantLikes}</span>
@@ -63,7 +90,6 @@ function VerComentario({nombreCompleto, fotoUser, fechaComentario, comentario, c
           </div>
         </div>
       </div>
-
     </div>
   );
 }
